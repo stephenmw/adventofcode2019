958db8b91d106e169d7859569eb738e2875baf84
Stephen Weinberg <stephenmweinberg@gmail.com>
2019-12-05 01:38:54 -0500

    Day 5 Part 1

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() {
-    let day = 4;
+    let day = 5;
     match day {
         1 => day1::main(),
         2 => day2::main(),
         3 => day3::main(),
         4 => day4::main(),
+        5 => day5::main(),
         _ => panic!("bad day"),
     };
 }
