a372d32d80f43b549ceecffb1d0dcbdaad053f1b
Stephen Weinberg <stephenmweinberg@gmail.com>
2019-12-06 01:20:01 -0500

    Day 6

---
+++ src/day6.rs
@@ -0,0 +1,73 @@
+use std::io;
+use std::fs;
+use std::path::Path;
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::VecDeque;
+
+
+pub fn main() {
+    let data = read_data("data/day6.txt").unwrap();
+    println!("Part 1: {}", count_orbits(&data));
+    println!("Part 2: {}", quickest_path_length(&data));
+}
+
+fn count_orbits(data: &[(String, String)]) -> usize {
+    let mut m: HashMap<&str, Vec<&str>> = HashMap::new();
+    data.iter().for_each(|(k, v)| m.entry(k).or_insert(Vec::new()).push(v));
+
+    let mut ret = 0;
+    let mut stack = vec![("COM", 0)];
+
+    while let Some((obj, count)) = stack.pop() {
+        ret += count;
+        m.get(obj)
+            .unwrap_or(&Vec::new())
+            .iter()
+            .for_each(|x| stack.push((x, count + 1)));
+    }
+
+    ret
+}
+
+fn quickest_path_length(data: &[(String, String)]) -> usize {
+    let mut m: HashMap<&str, Vec<&str>> = HashMap::new();
+    data.iter().for_each(|(k, v)| {
+        m.entry(k).or_insert(Vec::new()).push(v);
+        m.entry(v).or_insert(Vec::new()).push(k);
+    });
+
+    let start: &str = m.get("YOU").and_then(|x| x.first()).unwrap();
+    let end: &str = m.get("SAN").and_then(|x| x.first()).unwrap();
+
+    let mut frontier = VecDeque::new();
+    let mut seen = HashSet::new();
+    frontier.push_back((start, 0));
+    seen.insert(start);
+
+    while let Some((obj, count)) = frontier.pop_front() {
+        if obj == end {
+            return count;
+        }
+
+        for child in m.get(obj).unwrap() {
+            if seen.contains(child) {
+                continue
+            }
+            frontier.push_back((child, count + 1));
+            seen.insert(child);
+        }
+    }
+
+    unreachable!("no santa?");
+}
+
+fn read_data<P: AsRef<Path>>(filename: P) -> io::Result<Vec<(String, String)>> {
+    let raw = fs::read_to_string(filename)?;
+    Ok(raw.lines().map(|s|{
+        let mut iter = s.split_terminator(")");
+        let parent = iter.next().unwrap();
+        let child = iter.next().unwrap();
+        (parent.to_string(), child.to_string())
+    }).collect())
+}
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,17 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() {
-    let day = 5;
+    let day = 6;
     match day {
         1 => day1::main(),
         2 => day2::main(),
         3 => day3::main(),
         4 => day4::main(),
         5 => day5::main(),
+        6 => day6::main(),
         _ => panic!("bad day"),
     };
 }
