d6c82e0664ecf8a21ea049c02074c5cd84ac756f
Stephen Weinberg <stephenmweinberg@gmail.com>
2019-12-11 21:27:01 -0500

    day11: Part 2

--- src/day11.rs
+++ src/day11.rs
@@ -7,12 +7,59 @@ use std::convert::{TryFrom, TryInto};
 pub fn main() {
     let program = intcode::read_program("data/day11.txt").expect("failed to read program");
     println!("Part 1: {}", count_painted_tiles(&program));
+    part2(&program);
 }
 
 fn count_painted_tiles(program: &[i64]) -> usize {
-    let mut robot = Robot::new(program);
     let mut tiles = HashMap::new();
-    let mut loc = Point{x: 0, y: 0};
+    paint_tiles(program, &mut tiles, Point { x: 0, y: 0 });
+    tiles.len()
+}
+
+fn part2(program: &[i64]) {
+    let mut tiles = HashMap::new();
+    let start = Point { x: 0, y: 0 };
+    tiles.insert(start, Color::White);
+    paint_tiles(program, &mut tiles, start);
+
+    println!("{}", render_tiles(&tiles));
+}
+
+fn render_tiles(tiles: &HashMap<Point, Color>) -> String {
+    let min_x = tiles.keys().map(|p| p.x).min().unwrap();
+    let max_x = tiles.keys().map(|p| p.x).max().unwrap();
+    let min_y = tiles.keys().map(|p| p.y).min().unwrap();
+    let max_y = tiles.keys().map(|p| p.y).max().unwrap();
+
+    if min_x < 0 || min_y < 0 {
+        unimplemented!("negative x and y not implemented");
+    }
+
+    let x_len = max_x + 1;
+    let y_len = max_y + 1;
+
+    let mut data = vec![' ' as u8; (x_len * y_len) as usize];
+    let white_tiles = tiles
+        .iter()
+        .filter(|(_, v)| **v == Color::White)
+        .map(|(k, _)| k);
+
+    for p in white_tiles {
+        let i = (p.y * x_len + p.x) as usize;
+        data[i] = 'X' as u8;
+    }
+
+    let rows: Vec<_> = data
+        .chunks(x_len as usize)
+        .map(|x| String::from_utf8(x.to_vec()).unwrap())
+        .collect();
+
+    rows.join("\n")
+}
+
+fn paint_tiles(program: &[i64], tiles: &mut HashMap<Point, Color>, start: Point) {
+    let mut robot = Robot::new(program);
+    let mut loc = start;
 
     loop {
         let color = tiles.get(&loc).cloned().unwrap_or(Color::Black);
@@ -28,8 +75,6 @@ fn count_painted_tiles(program: &[i64]) -> usize {
             None => break,
         }
     }
-
-    tiles.len()
 }
 
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
@@ -72,7 +117,7 @@ impl Robot {
         self.direction = match turn {
             0 => self.direction.left(),
             1 => self.direction.right(),
-            _ => panic!("unknown turn")
+            _ => panic!("unknown turn"),
         };
 
         let new_loc = self.direction.step(loc);
@@ -128,10 +173,10 @@ impl Direction {
 
     fn step(&self, p: &Point) -> Point {
         match self {
-            Direction::North => Point {x: p.x, y: p.y - 1},
-            Direction::West => Point {x: p.x + 1, y: p.y},
-            Direction::South => Point {x: p.x, y: p.y + 1},
-            Direction::East => Point {x: p.x - 1, y: p.y},
+            Direction::North => Point { x: p.x, y: p.y - 1 },
+            Direction::West => Point { x: p.x + 1, y: p.y },
+            Direction::South => Point { x: p.x, y: p.y + 1 },
+            Direction::East => Point { x: p.x - 1, y: p.y },
         }
     }
 }
